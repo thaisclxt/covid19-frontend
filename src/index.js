96422f9c318fac7c1cb8d4923aa13f80c6c0934f
thais.calixto <thais.calixto@mail.uft.edu.br>
2022-04-21 16:52:25 -0300

    rename pp.js to home.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import App from "./App";
+import Home from "./pages/Home";
 import Patients from "./routes/patients";
 import Schedule from "./routes/schedule";
 
@@ -9,7 +9,7 @@ const rootElement = document.getElementById("root");
 render(
 	<BrowserRouter>
 		<Routes>
-			<Route path="/" element={<App />} />
+			<Route path="/" element={<Home />} />
 			<Route path="schedule" element={<Schedule />} />
 			<Route path="patients" element={<Patients />} />
 		</Routes>
