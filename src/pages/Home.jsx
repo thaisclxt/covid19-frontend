5cbe3aebbc73d377c59e4b872446fdc53f4ad2a8
thais.calixto <thais.calixto@mail.uft.edu.br>
2022-04-21 17:40:56 -0300

    use appshell with header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,28 @@
+import { AppShell, Header, Text } from "@mantine/core";
 import { Link } from "react-router-dom";
 
-const Home = () => (
-	<div className="App">
-		<h1>Vacinação Covid-19</h1>
-		<nav>
-			<Link to="/schedule">Agendar vacina</Link> |{" "}
-			<Link to="/patients">Visualizar agendamentos</Link>
-		</nav>
-	</div>
-);
+const Home = () => {
+	return (
+		<AppShell
+			padding="md"
+			header={
+				<Header height={70} p="md">
+					<div
+						style={{ display: "flex", alignItems: "center", height: "100%" }}
+					>
+						<Text>Vacinação Covid-19</Text>
+					</div>
+				</Header>
+			}
+		>
+			{
+				<nav>
+					<Link to="/schedule">Agendar vacina</Link> |{" "}
+					<Link to="/patients">Visualizar agendamentos</Link>
+				</nav>
+			}
+		</AppShell>
+	);
+};
 
 export default Home;
