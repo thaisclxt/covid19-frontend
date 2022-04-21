2a9f472add8925b12231308939201525c5f61bbb
thais.calixto <thais.calixto@mail.uft.edu.br>
2022-04-21 17:13:07 -0300

    extract Router

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import { render } from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
-import Patients from "./pages/Patients";
-import Schedule from "./pages/Schedule";
+import Router from "./Router";
 
 const rootElement = document.getElementById("root");
-render(
-	<BrowserRouter>
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="schedule" element={<Schedule />} />
-			<Route path="patients" element={<Patients />} />
-		</Routes>
-	</BrowserRouter>,
-	rootElement
-);
+render(<Router />, rootElement);
