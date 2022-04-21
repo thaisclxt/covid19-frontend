1375c14ebf4bab78749e732455830ab0da846f63
thais.calixto <thais.calixto@mail.uft.edu.br>
2022-04-21 20:22:53 -0300

    modify theme

---
+++ src/theme/index.js
@@ -0,0 +1,21 @@
+const myTheme = {
+	colors: {
+		brand: [
+			"#F0BBDD",
+			"#ED9BCF",
+			"#EC7CC3",
+			"#ED5DB8",
+			"#F13EAF",
+			"#F71FA7",
+			"#52040F",
+		],
+	},
+	primaryColor: "brand",
+	fontFamily: "Josefin Sans",
+	defaultRadius: 30,
+	spacing: {
+		xl: 40,
+	},
+};
+
+export default myTheme;
