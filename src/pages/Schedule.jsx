e3d3a95b5bc46f2699753e571c1392a9200f5804
thais.calixto <thais.calixto@mail.uft.edu.br>
2022-04-22 12:52:04 -0300

    create form component

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,6 +1,20 @@
+import { Text, Space, Center, Paper, Input } from "@mantine/core";
+
 const Schedule = () => (
 	<main>
-		<h2>Agendar vacina</h2>
+		<Text size="xl" weight="bold" align="center">
+			Agendar Vacina
+		</Text>
+		<Space h="md" />
+		<Center>
+			<Paper
+				shadow="md"
+				p="xl"
+				style={{ width: "40%", backgroundColor: "#52040F" }}
+			>
+				<Input placeholder="Nome" size="md" />
+			</Paper>
+		</Center>
 	</main>
 );
 
