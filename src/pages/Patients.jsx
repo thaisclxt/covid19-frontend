0f9db82bed8cc791b0b25fe41d3566307357b0d7
thais.calixto <thais.calixto@mail.uft.edu.br>
2022-04-21 23:01:28 -0300

    create table component

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -1,7 +1,30 @@
+import { Table } from "@mantine/core";
+
 const Patients = () => (
-	<main>
-		<h2>Visualizar agendamentos</h2>
-	</main>
+	<Table highlightOnHover>
+		<thead>
+			<tr>
+				<th>Nome do pasciente</th>
+				<th>Data de nascimento</th>
+				<th>Dia da vacina</th>
+				<th>Horário da vacina</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Thaís</td>
+				<td>06/12/200</td>
+				<td>22/04/2022</td>
+				<td>15h39min</td>
+			</tr>
+			<tr>
+				<td>Thaís</td>
+				<td>06/12/200</td>
+				<td>22/04/2022</td>
+				<td>15h39min</td>
+			</tr>
+		</tbody>
+	</Table>
 );
 
 export default Patients;
