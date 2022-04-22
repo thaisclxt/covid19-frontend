34b63344b8594751b120255fd9e04163cac88685
thais.calixto <thais.calixto@mail.uft.edu.br>
2022-04-22 12:00:37 -0300

    use Table on Paper

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -1,30 +1,40 @@
-import { Table } from "@mantine/core";
+import { Text, Table, Paper, Space, Center } from "@mantine/core";
 
 const Patients = () => (
-	<Table highlightOnHover>
-		<thead>
-			<tr>
-				<th>Nome do pasciente</th>
-				<th>Data de nascimento</th>
-				<th>Dia da vacina</th>
-				<th>Horário da vacina</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td>Thaís</td>
-				<td>06/12/200</td>
-				<td>22/04/2022</td>
-				<td>15h39min</td>
-			</tr>
-			<tr>
-				<td>Thaís</td>
-				<td>06/12/200</td>
-				<td>22/04/2022</td>
-				<td>15h39min</td>
-			</tr>
-		</tbody>
-	</Table>
+	<main>
+		<Text size="xl" weight="bold" align="center">
+			Agendamentos
+		</Text>
+		<Space h="md" />
+		<Center>
+			<Paper shadow="md" p="xl" style={{ width: "80%" }}>
+				<Table highlightOnHover>
+					<thead>
+						<tr>
+							<th>Nome do pasciente</th>
+							<th>Data de nascimento</th>
+							<th>Dia da vacina</th>
+							<th>Horário da vacina</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Thaís</td>
+							<td>06/12/200</td>
+							<td>22/04/2022</td>
+							<td>15h39min</td>
+						</tr>
+						<tr>
+							<td>Thaís</td>
+							<td>06/12/200</td>
+							<td>22/04/2022</td>
+							<td>15h39min</td>
+						</tr>
+					</tbody>
+				</Table>
+			</Paper>
+		</Center>
+	</main>
 );
 
 export default Patients;
