15886e587b25eacf70189beae52b8f89b9724642
thais.calixto <thais.calixto@mail.uft.edu.br>
2022-04-22 16:04:26 -0300

    create constant patients

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -1,40 +1,46 @@
 import { Text, Table, Paper, Space, Center } from "@mantine/core";
 
-const Patients = () => (
-	<main>
-		<Text size="xl" weight="bold" align="center" color="#52040F">
-			Agendamentos
-		</Text>
-		<Space h="md" />
-		<Center>
-			<Paper shadow="md" p="xl" style={{ width: "80%" }}>
-				<Table highlightOnHover>
-					<thead>
-						<tr>
-							<th>Nome do pasciente</th>
-							<th>Data de nascimento</th>
-							<th>Dia da vacina</th>
-							<th>Horário da vacina</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>Thaís</td>
-							<td>06/12/200</td>
-							<td>22/04/2022</td>
-							<td>15h39min</td>
-						</tr>
-						<tr>
-							<td>Thaís</td>
-							<td>06/12/200</td>
-							<td>22/04/2022</td>
-							<td>15h39min</td>
-						</tr>
-					</tbody>
-				</Table>
-			</Paper>
-		</Center>
-	</main>
-);
+const Patients = () => {
+	const patients = [
+		{
+			name: "Thaís",
+			birthDate: "06/12/2000",
+			vaccineDate: "06/12/2000",
+			vaccineHour: "00:00",
+		},
+	];
+	return (
+		<main>
+			<Text size="xl" weight="bold" align="center" color="#52040F">
+				Agendamentos
+			</Text>
+			<Space h="md" />
+			<Center>
+				<Paper shadow="md" p="xl" style={{ width: "80%" }}>
+					<Table highlightOnHover>
+						<thead>
+							<tr>
+								<th>Nome do pasciente</th>
+								<th>Data de nascimento</th>
+								<th>Dia da vacina</th>
+								<th>Horário da vacina</th>
+							</tr>
+						</thead>
+						<tbody>
+							{patients.map((patient, index) => (
+								<tr key={index}>
+									<td>{patient.name}</td>
+									<td>{patient.birthDate}</td>
+									<td>{patient.vaccineDate}</td>
+									<td>{patient.vaccineHour}</td>
+								</tr>
+							))}
+						</tbody>
+					</Table>
+				</Paper>
+			</Center>
+		</main>
+	);
+};
 
 export default Patients;
