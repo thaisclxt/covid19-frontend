2fb44b054988667b96321374b92e822cf6bf9278
thais.calixto <thais.calixto@mail.uft.edu.br>
2022-04-23 16:05:33 -0300

    put bracket on Schedule function

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -4,51 +4,54 @@ import { Calendar, LetterCase, Vaccine } from "tabler-icons-react";
 
 import "dayjs/locale/pt-br";
 
-const Schedule = () => (
-	<main>
-		<Text size="xl" weight="bold" align="center" color="#52040F">
-			Agendar Vacina
-		</Text>
-		<Space h="md" />
-		<Center>
-			<Paper shadow="md" p="xl" style={{ width: "40%" }}>
-				<TextInput
-					label="Nome"
-					required
-					mb={12}
-					icon={<LetterCase size={16} color="#52040F" />}
-				/>
-				<DatePicker
-					icon={<Calendar size={16} color="#52040F" />}
-					label="Data de nascimento"
-					firstDayOfWeek="sunday"
-					inputFormat="DD/MM/YYYY"
-					locale="pt-br"
-					required
-					clearable
-					mb={12}
-				/>
-				<DatePicker
-					icon={<Vaccine size={16} color="#52040F" />}
-					label="Data da vacina"
-					firstDayOfWeek="sunday"
-					inputFormat="DD/MM/YYYY"
-					locale="pt-br"
-					required
-					clearable
-					mb={12}
-				/>
-				<TimeInput
-					icon={<Vaccine size={16} color="#52040F" />}
-					label="Horário da vacina"
-					required
-					clearable
-					mb={12}
-				/>
-				<Button>Confirmar agendamento</Button>
-			</Paper>
-		</Center>
-	</main>
-);
+const Schedule = () => {
+	return (
+		<main>
+			<Text size="xl" weight="bold" align="center" color="#52040F">
+				Agendar Vacina
+			</Text>
+			<Space h="md" />
+			<Center>
+				<Paper shadow="md" p="xl" style={{ width: "40%" }}>
+					<TextInput
+						label="Nome"
+						required
+						mb={12}
+						icon={<LetterCase size={16} color="#52040F" />}
+						value={value}
+					/>
+					<DatePicker
+						icon={<Calendar size={16} color="#52040F" />}
+						label="Data de nascimento"
+						firstDayOfWeek="sunday"
+						inputFormat="DD/MM/YYYY"
+						locale="pt-br"
+						required
+						clearable
+						mb={12}
+					/>
+					<DatePicker
+						icon={<Vaccine size={16} color="#52040F" />}
+						label="Data da vacina"
+						firstDayOfWeek="sunday"
+						inputFormat="DD/MM/YYYY"
+						locale="pt-br"
+						required
+						clearable
+						mb={12}
+					/>
+					<TimeInput
+						icon={<Vaccine size={16} color="#52040F" />}
+						label="Horário da vacina"
+						required
+						clearable
+						mb={12}
+					/>
+					<Button onclick={onSubmit}>Confirmar agendamento</Button>
+				</Paper>
+			</Center>
+		</main>
+	);
+};
 
 export default Schedule;
